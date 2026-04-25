bd19b2d19931c568c956d8049f373735b6fe8256
erikcerne <erik.cerne2@gmail.com>
2026-04-25 17:37:44 +0200

    synk

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { Auth0Provider } from "@auth0/auth0-react";
+const domainEnv = import.meta.env.VITE_AUTH0_DOMAIN;
+const clientIdEnv = import.meta.env.VITE_AUTH0_CLIENT_ID;
+const audienceEnv = import.meta.env.VITE_AUTH0_AUDIENCE;
 
-createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById("root")!;
+const root = createRoot(rootElement);
+
+root.render(
   <StrictMode>
-    <App />
+    <Auth0Provider
+      domain={domainEnv}
+      clientId={clientIdEnv}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+        audience: audienceEnv,
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </StrictMode>,
-)
+);
