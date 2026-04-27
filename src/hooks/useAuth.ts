29d7c8d629657499bdf6c0f7862d1515124359da
erikcerne <erik.cerne2@gmail.com>
2026-04-27 14:36:19 +0200

    mov login to sepret file

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,46 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import { useEffect } from "react";
+
+const baseUrl = import.meta.env.VITE_API_BASE_URL;
+
+export const useAuth = () => {
+  const {
+    isAuthenticated,
+    loginWithRedirect,
+    logout,
+    user,
+    getAccessTokenSilently,
+    isLoading,
+  } = useAuth0();
+
+  useEffect(() => {
+    const syncUser = async () => {
+      if (isAuthenticated) {
+        try {
+          const token = await getAccessTokenSilently();
+          await fetch(`${baseUrl}/users/register`, {
+            method: "POST",
+            headers: {
+              Authorization: `Bearer ${token}`,
+              "Content-Type": "application/json",
+            },
+          });
+          console.log("Användare synkad med backend");
+        } catch (error) {
+          console.error("Synkning misslyckades:", error);
+        }
+      }
+    };
+
+    syncUser();
+  }, [isAuthenticated, getAccessTokenSilently]);
+
+  return {
+    isAuthenticated,
+    login: loginWithRedirect,
+    logout: () => logout({ logoutParams: { returnTo: window.location.origin } }),
+    user,
+    isLoading,
+    getAccessTokenSilently
+  };
+};
